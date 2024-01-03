b0de7e0faee9f3516feecd1dc2567ae18eee6ca5
jaelinny <148875683+Jaelinny@users.noreply.github.com>
2024-01-04 02:24:56 +0900

    Update BbsController.java

--- src/main/java/bbs/controller/BbsController.java
+++ src/main/java/bbs/controller/BbsController.java
@@ -4,28 +4,18 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-//import org.springframework.ui.Model;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import bbs.dto.BbsDTO;
 import bbs.service.BbsService;
-//import bbs.service.BbsService;
-//import bbs.service.BbsServiceImpl;
 
 @Controller
 public class BbsController { 
 	
 	@Autowired
 	private BbsService bbsService; // 
-	
-	@RequestMapping("/bbs/openBbsList.do")
-//	@GetMapping("/") // 
-//	public String openBoardList(Model model)  { //
-		
+
 	public ModelAndView openBbsList() throws Exception{
 		ModelAndView mv = new ModelAndView("/bbs/bbsList");
 		
