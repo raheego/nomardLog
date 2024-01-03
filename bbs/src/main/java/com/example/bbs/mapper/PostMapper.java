55b779460234f7f4de4d9986c01f3b2329d9ba9d
ldh0308 <142721325+ldh0308@users.noreply.github.com>
2024-01-03 21:51:45 +0900

    Delete bbs directory

--- bbs/src/main/java/com/example/bbs/mapper/PostMapper.java
+++
@@ -1,9 +0,0 @@
-package com.example.bbs.mapper;
-
-import com.example.bbs.domain.Post;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface PostMapper {
-    void insertPost(Post post);
-}
