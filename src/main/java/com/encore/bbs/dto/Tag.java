25a99d5c45a65c1254736c17e58147cf3da5952e
ldh0308 <142721325+ldh0308@users.noreply.github.com>
2024-01-04 16:12:52 +0900

    add : dto 추가 생성

---
+++ src/main/java/com/encore/bbs/dto/Tag.java
@@ -0,0 +1,13 @@
+package com.encore.bbs.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Tag {
+    private Long tagId;
+    private String content;
+}
