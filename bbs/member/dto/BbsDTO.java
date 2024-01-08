d5d39bd41afcb8a39711580aea4fdbd50397f10b
papercrane55123 <150432433+papercrane55123@users.noreply.github.com>
2024-01-08 13:45:54 +0900

    Add files via upload

---
+++ bbs/member/dto/BbsDTO.java
@@ -0,0 +1,20 @@
+package com.encore.bbs.member.dto;
+
+import java.time.LocalDateTime;
+
+import lombok.Data;
+
+@Data
+public class BbsDTO {
+
+	private int bbsId;
+	private int memberCode;
+	private String title;
+	private String contents;
+	private LocalDateTime createdAt;
+	private LocalDateTime modifyAt;
+	private int viewCnt;
+	private int likeCnt;
+	private int tagId;
+	private boolean isDeleted;
+}
