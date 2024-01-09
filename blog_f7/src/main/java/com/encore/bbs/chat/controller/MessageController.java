478c081f63360055ac6c3d9b7c30e4cee6f0dfa1
papercrane55123 <gyeongmin55@naver.com>
2024-01-09 17:14:42 +0900

    보냅니다

---
+++ blog_f7/src/main/java/com/encore/bbs/chat/controller/MessageController.java
@@ -0,0 +1,23 @@
+package com.encore.bbs.chat.controller;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.stereotype.Controller;
+
+import com.encore.bbs.chat.model.ChatMessage;
+
+@RequiredArgsConstructor
+@Controller
+public class MessageController {
+
+    private final SimpMessageSendingOperations messagingTemplate;
+
+    @MessageMapping("/chat/message")
+    public void message(ChatMessage message) {
+        if (ChatMessage.MessageType.ENTER.equals(message.getType()))
+            message.setMessage(message.getSender() + "님이 입장하셨습니다.");
+        messagingTemplate.convertAndSend("/topic/chat/room/"+message.getRoomId(),message);
+    }
+}
